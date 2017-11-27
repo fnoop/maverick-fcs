330a6d64ef0fd983131253568ac83b220c150d0b
fnoop <fnoopdogg@gmail.com>
2017-11-27 14:28:52 +0000

    Add description to logfile uploader #691

--- tornado/file_upload.py
+++ tornado/file_upload.py
@@ -4,6 +4,7 @@
 import tornado
 import tornado.ioloop
 import tornado.web
+import sqlite3
 import os, uuid
 
 # Define paths
@@ -18,16 +19,37 @@ class Userform(tornado.web.RequestHandler):
 class Upload(tornado.web.RequestHandler):
     def post(self):
         fileinfo = self.request.files['file'][0]
-        #print "fileinfo is", fileinfo
-        fname = fileinfo['filename']
+        filename = fileinfo['filename']
         anon = self.get_argument("anon", None, False)
         if anon:
             _uploadDir = anonybox
         else:
             _uploadDir = inbox
-        fh = open(os.path.join(_uploadDir, fname), 'w')
+
+        # Write an entry to the meta db
+        description = self.get_argument("description", None, False)
+        try:
+            conn = sqlite3.connect("/srv/maverick/data/analysis/mavlog-meta.db")
+            cursor = conn.cursor()
+            # See if entry already exists
+            cursor.execute("SELECT id FROM logfiles WHERE filename='"+filename+"'")
+            try:
+                id_exists = cursor.fetchone()[0]
+            except:
+                id_exists = None
+            if not id_exists:
+                cursor.execute('INSERT INTO logfiles (filename,state,description) VALUES (?,?,?)', (filename,"uploaded",description))
+            else:
+                cursor.execute('UPDATE logfiles SET start=?,state=?,description=? WHERE id=?', (filename,"uploaded",description,id_exists))
+            conn.commit()
+            conn.close()
+        except Exception as e:
+            print "Meta entry exception: {}".format(repr(e))
+
+        # Write the upload contents to a new file in inbox/anonybox
+        fh = open(os.path.join(_uploadDir, filename), 'w')
         fh.write(fileinfo['body'])
-        self.render("index.html", fname=fname, _uploadDir=_uploadDir)
+        self.render("index.html", fname=filename, _uploadDir=_uploadDir)
 
 application = tornado.web.Application([
         (r"/", Userform),
